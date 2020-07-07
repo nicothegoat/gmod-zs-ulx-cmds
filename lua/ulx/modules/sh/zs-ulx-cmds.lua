d6efccb88ce8b84cf4ba9f00469da1b8014ef619
NicoTheGoat <minecrafter2010@gmail.com>
2020-07-07 21:07:03 +0200

    Add `forceboss` command

--- lua/ulx/modules/sh/zs-ulx-cmds.lua
+++ lua/ulx/modules/sh/zs-ulx-cmds.lua
@@ -18,4 +18,39 @@ end
 local redeem = ulx.command( "ZS ULX Commands", "ulx redeem", ulx.redeem, "!redeem" )
 redeem:addParam{ type = ULib.cmds.PlayersArg }
 redeem:addParam{ type = ULib.cmds.BoolArg, default = false, hint = "silent" }
-redeem:addHelp( "Redeem target(s)." )
+redeem:addHelp( "Redeem target(s)" )
+
+
+local function ulx.forceboss( caller, targets, silent, inPlace )
+	local affected = {}
+
+	for i = 1, #targets do
+		local target = targets[ i ]
+
+		if target:Team == TEAM_UNDEAD then
+			if inPlace then
+				local pos = target:GetPos()
+				local ang = target:GetAngles()
+
+				gamemode.Call( "SpawnBossZombie", target )
+
+				target:SetPos( pos )
+				target:SetAngles( ang )
+			else
+				gamemode.Call( "SpawnBossZombie", target )
+			end
+
+			table.insert( affected, target )
+		else
+			ULib.tsayError( caller, target:Nick() .. " isn't a zombie!", true )
+		end
+	end
+
+	ulx.fancyLogAdmin( caller, "#A forced #T to be boss", affected )
+end
+
+local forceboss = ulx.command( "ZS ULX Commands", "ulx forceboss", ulx.forceboss, "!forceboss" )
+forceboss:addParam{ type = ULib.cmds.PlayersArg }
+forceboss:addParam{ type = ULib.cmds.BoolArg, default = false, hint = "silent" }
+forceboss:addParam{ type = ULib.cmds.BoolArg, default = false, hint = "respawn in place" }
+forceboss:addHelp( "Respawn target(s) as boss" )
