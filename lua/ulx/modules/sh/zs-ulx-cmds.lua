f8451f151da8fb1efeeeedb5a27da504144acf96
NicoTheGoat <minecrafter2010@gmail.com>
2020-07-13 01:01:41 +0200

    Remove `silent` argument from `redeem`
    Didn't seem to do anything of value
    ( just like me :) )

--- lua/ulx/modules/sh/zs-ulx-cmds.lua
+++ lua/ulx/modules/sh/zs-ulx-cmds.lua
@@ -1,11 +1,11 @@
-function ulx.redeem( caller, targets, silent )
+function ulx.redeem( caller, targets )
 	local affected = {}
 
 	for i = 1, #targets do
 		local target = targets[ i ]
 
 		if target:Team() == TEAM_UNDEAD then
-			target:Redeem( silent )
+			target:Redeem()
 			table.insert( affected, target )
 		else
 			ULib.tsayError( caller, target:Nick() .. " isn't a zombie!", true )
@@ -17,7 +17,6 @@ end
 
 local redeem = ulx.command( "ZS ULX Commands", "ulx redeem", ulx.redeem, "!redeem" )
 redeem:addParam{ type = ULib.cmds.PlayersArg }
-redeem:addParam{ type = ULib.cmds.BoolArg, default = false, hint = "silent", ULib.cmds.optional }
 redeem:defaultAccess( ULib.ACCESS_ADMIN )
 redeem:help( "Redeem target(s)" )
 
